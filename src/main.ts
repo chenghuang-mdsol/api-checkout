cfe008ef36807d79f69ecdcdb134a2377e50d65e
Cheng Huang <chuang@mdsol.com>
2022-03-31 19:24:10 -0400

    Init

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as io from '@actions/io'
+import * as path from 'path'
+import {Octokit} from '@octokit/rest'
+import {getFiles} from './functions'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const repository: string = core.getInput('repository')
+    const outDirectory: string = core.getInput('out-directory')
+    const paths: string[] = core.getInput('paths').split('\r?\n')
+    const githubToken: string = core.getInput('github-token')
+    const refs: string = core.getInput('refs')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    await getFiles(repository, paths, refs, githubToken, outDirectory);
 
-    core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
